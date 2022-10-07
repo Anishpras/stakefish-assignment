3110186728e2fe56007abc70322345bf51826ced
anishpras <anishprashun118@gmail.com>
2022-10-07 14:28:05 +0530

    arrow button atoms created

--- src/components/molecules/PageChanger/index.tsx
+++ src/components/molecules/PageChanger/index.tsx
@@ -1,3 +1,5 @@
+import BackArrowButton from '@/components/atoms/BackArrowButton';
+import ForwardArrowButton from '@/components/atoms/ForwardArrowButton';
 import Link from 'next/link';
 import React from 'react';
 
@@ -11,36 +13,13 @@ const PageChanger = ({ pageNumber }: Props) => {
       <div className="py-10 ">
         <Link href={pageNumber !== 2 ? `/exchanges/${pageNumber - 1}` : `/`}>
           <button className="mx-4 rounded-lg bg-gray-200 p-2 ">
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 12 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="rotate-180"
-            >
-              <path
-                d="M6.18182 11.4205L5.01136 10.2614L8.51705 6.75568H0V5.0625H8.51705L5.01136 1.5625L6.18182 0.397727L11.6932 5.90909L6.18182 11.4205Z"
-                fill="#000"
-              />
-            </svg>
+            <BackArrowButton />
           </button>
         </Link>
         {pageNumber.toString()}
         <Link href={`/exchanges/${pageNumber + 1}`}>
           <button className="mx-4 rounded-lg bg-gray-200 p-2 ">
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 12 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M6.18182 11.4205L5.01136 10.2614L8.51705 6.75568H0V5.0625H8.51705L5.01136 1.5625L6.18182 0.397727L11.6932 5.90909L6.18182 11.4205Z"
-                fill="#000"
-              />
-            </svg>
+            <ForwardArrowButton />
           </button>
         </Link>
       </div>
@@ -51,18 +30,7 @@ const PageChanger = ({ pageNumber }: Props) => {
         {1}
         <Link href="/exchanges/2">
           <button className="mx-4 rounded-lg bg-gray-200 p-2 ">
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 12 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M6.18182 11.4205L5.01136 10.2614L8.51705 6.75568H0V5.0625H8.51705L5.01136 1.5625L6.18182 0.397727L11.6932 5.90909L6.18182 11.4205Z"
-                fill="#000"
-              />
-            </svg>
+            <ForwardArrowButton />
           </button>
         </Link>
       </div>
